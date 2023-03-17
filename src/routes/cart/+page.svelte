0249351e2dba8d36ec56dd29d23ac779b5282866
Tyler Pillay <treshpillay@gmail.com>
2023-03-17 15:14:05 +0200

    move checkout button

--- src/routes/cart/+page.svelte
+++ src/routes/cart/+page.svelte
@@ -7,11 +7,12 @@
   <meta name="description" content="Cart page">
 </svelte:head>
 
-<h2>Cart Total: R{$cartTotal}</h2>
-<a href="/payment">
-  <button>Checkout</button>
-</a>
-
+<div class="totalContainer">
+  <h2>Cart Total: R{$cartTotal}</h2>
+  <a href="/payment">
+    <button>Checkout</button>
+  </a>
+</div>
 
 <div class="grid">
   {#each $cart.products as product}
@@ -26,6 +27,12 @@
 </div>
 
 <style>
+    .totalContainer {
+        display: flex;
+        justify-content: space-between;
+        padding: 20px;
+    }
+
     p {
         margin-top: 10px;
     }
@@ -50,5 +57,19 @@
     .product-image {
         max-width: 100%;
         height: 50%;
+    }
+
+    a button {
+        background-color: #4CAF50;
+        width: fit-content;
+        border: none;
+        color: white;
+        padding: 15px 32px;
+        text-align: center;
+        text-decoration: none;
+        display: inline-block;
+        font-size: 16px;
+        margin: 4px 2px;
+        cursor: pointer;
     }
 </style>
